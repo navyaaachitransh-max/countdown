b662caa45a887ae9a110ae58750619e9527ed790
navyaaachitransh-max <navyaaachitransh@gmail.com>
2026-06-07 22:26:00 +0530

    Add CSS styles to script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Kairos");
+body{
+  background:black;
+  color:white;
+
+  display:flex;
+  flex-direction:column;
+
+  justify-content:center;
+  align-items:center;
+
+  height:100vh;
+
+  font-family:Georgia;
+}
+
+h1{
+  font-size:5rem;
+  color:gold;
+}
+
+#countdown{
+  font-size:3rem;
+}
